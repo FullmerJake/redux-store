c888b8a9bdfd6bcdc68688a785b50ef4ff9169cd
Jake Fullmer <w.jake.fullmer@gmail.com>
2021-01-18 00:03:11 -0700

    bug fixes

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,56 +1,128 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+
+// import { /*UPDATE_CATEGORIES,*/ UPDATE_CURRENT_CATEGORY } from '../../utils/actions.js';
+
 import { useQuery } from '@apollo/react-hooks';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
-import { QUERY_CATEGORIES } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
+import { QUERY_CATEGORIES } from "../../utils/queries";
+
+// import { useStoreContext } from '../../utils/GlobalState.js';
+import { idbPromise } from '../../utils/helpers.js';
+
+//REDUX IMPORTS 
+import {
+  updateCategories, updateCurrentCategory
+} from '../../actions';
+
+import { useSelector, useDispatch } from 'react-redux'
 
 function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
 
-  const { categories } = state;
+  //REDUX
+  // observe global commerce state
+  const commerceState = useSelector(state => state.commerce);
+  //use global action dispatcher
+  const dispatchREDUX = useDispatch();
 
+  console.log('VVV CHECKING GLOBAL STATE IN CATEGORY MENU COMPONENT VVV');
+  console.log(commerceState);
+
+  
+  // const [, dispatch] = useStoreContext();
+  // const {categories} = state;
+
+  const {//GET REDUX CATEGORIES
+    categories
+  } = commerceState;
+  const categoriesREDUX = categories;
+
+  //GRAPHQL DATABASE GET CATEGORIES QUERY
+  //destructure the categoryData property from the data returned from GRAPHQL
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
+    // if categoryData exists or has changed from the response
+    // of useQuery, then run dispatch()
     if (categoryData) {
-      dispatch({
-        type: UPDATE_CATEGORIES,
-        categories: categoryData.categories
-      });
+      //execute our dispatch function with our action object
+      // indicating the type of action and the data to set our state
+      // for categories to something new
+      // dispatch
+      // (
+      //   {
+      //     type: UPDATE_CATEGORIES,
+      //     categories: categoryData.categories
+      //   }
+      // );
+      //REDUX DISPATCHER
+      dispatchREDUX(updateCategories(categoryData.categories))
+
+      //also save categories to idb
       categoryData.categories.forEach(category => {
         idbPromise('categories', 'put', category);
       });
-    } else if (!loading) {
-      idbPromise('categories', 'get').then(categories => {
-        dispatch({
-          type: UPDATE_CATEGORIES,
-          categories: categories
-        });
-      });
-    }
-  }, [categoryData, loading, dispatch]);
+    } else if (!loading) {//if server is offline get categories from idb
+      idbPromise('categories', 'get')
+      .then
+      (
+        categories => 
+        {
+          // dispatch
+          // (
+          //   {
+          //     type: UPDATE_CATEGORIES,
+          //     categories: categories
+          //   }
+          // );
 
-  const handleClick = id => {
-    dispatch({
-      type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: id
-    });
+          //REDUX UPDATE CATEGORIES DISPATCH
+          dispatchREDUX(updateCategories(categories));
+        }
+      );
+    }
+  }, [categoryData, loading, dispatchREDUX]);
+
+  const handleClick = _id => {
+    // dispatch
+    // (
+    //   {
+    //     type: UPDATE_CURRENT_CATEGORY,
+    //     currentCategory: id
+    //   }
+    // );
+    dispatchREDUX(updateCurrentCategory(_id));
   };
+
+  const setCurrentCategoryBlank = () => {
+    // dispatch
+    // (
+    //   {
+    //     type: UPDATE_CURRENT_CATEGORY,
+    //     currentCategory: ''
+    //   }
+    // );
+    dispatchREDUX(updateCurrentCategory(''));
+  }
 
   return (
     <div>
       <h2>Choose a Category:</h2>
-      {categories.map(item => (
-        <button
-          key={item._id}
-          onClick={() => {
-            handleClick(item._id);
-          }}
-        >
-          {item.name}
-        </button>
-      ))}
+      <button
+        onClick={setCurrentCategoryBlank}
+      >
+        All Categories
+      </button>
+      {
+        categoriesREDUX.map(category => (
+          <button
+            key={category._id}
+            onClick={() => {
+              handleClick(category._id)
+            }}
+          >
+            {category.name}
+          </button>
+        ))
+      }
     </div>
   );
 }
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,67 +1,133 @@
 import React, { useEffect } from "react";
-import ProductItem from "../ProductItem";
-import { useStoreContext } from "../../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../../utils/actions";
 import { useQuery } from '@apollo/react-hooks';
+import { idbPromise } from '../../utils/helpers.js';
+
+// import { useStoreContext } from '../../utils/GlobalState.js';
+//import { UPDATE_PRODUCTS } from '../../utils/actions.js';
+
+import ProductItem from "../ProductItem";
 import { QUERY_PRODUCTS } from "../../utils/queries";
-import { idbPromise } from "../../utils/helpers";
 import spinner from "../../assets/spinner.gif"
 
+//REDUX
+import { useSelector, useDispatch } from 'react-redux';
+//REDUX ACTIONS
+import {
+  updateProducts
+} from '../../actions';
+
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+  //retrieve the global state object and dispatch method to update state
+  // const [, dispatch] = useStoreContext();
+  // const { currentCategory } = state;
 
-  const { currentCategory } = state;
+  //GRAPHQL DATABASE QUERY get all products
+  const { loading, data } = useQuery(QUERY_PRODUCTS);//query data from database
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+  //REDUX OBSERVE GLOBAL STATE OF THE GLOBAL REDUCER
+  const commerceState = useSelector(state => state.commerce);
+  //console.log('vvvv GLOBAL COMMERCE STATE vvvv');
+  //console.log(commerceState);
 
+  //REDUX USE DISPATCHER
+  const dispatchREDUX = useDispatch();
+
+  //GET PIECE OF GLOBAL STATE TO OBSERVE
+  const {
+    products,
+    currentCategory
+    // cartOpen,
+    // categories,
+  } = commerceState;
+
+  ///console.log('vvvv REDUX PRODUCTS ARRAY vvvv');
+  ///console.log(products);
+  
   useEffect(() => {
-    if(data) {
-      dispatch({
-           type: UPDATE_PRODUCTS,
-          products: data.products
-        });
-        data.products.forEach((product) => {
-          idbPromise('products', 'put', product);
-        });
-    } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
-        dispatch({
-          type: UPDATE_PRODUCTS,
-         products: products
-       });
-      });
-    }
-  }, [data, loading, dispatch]);
+    //if theres data to be stored
+    if (data) {
+      //lets store it in the global state object
+      // dispatch
+      // (
+      //   {
+      //     type: UPDATE_PRODUCTS,
+      //     products: data.products
+      //   }
+      // );
 
+      //REDUX STORE INTO GLOBAL STATE BY USING THE
+      // DISPATCH ACTION - updateProducts
+      dispatchREDUX(updateProducts(data.products));
+      
+      //also take each product and save it to idb using helper function
+      data.products.forEach(product => {
+        idbPromise('products', 'put', product);
+      });
+      //add else if to check if `loading is falsey in `useQuery()`
+    } else if (!loading) {
+      //since server is offline, get all of the data from the `products` store
+      idbPromise('products', 'get')
+      .then
+      (
+        products => 
+        {
+          //use retrieved data to set global state for offline browsing
+          // dispatch
+          // (
+          //   {
+          //     type: UPDATE_PRODUCTS,
+          //     products: products
+          //   }
+          // );
+          
+          //redux dispatch
+          dispatchREDUX(updateProducts(products))
+        }
+      );
+    }
+  }, [data, loading, dispatchREDUX])
+
+
+  //NOW GETTING REDUX commerceState.currentCategory STATE
   function filterProducts() {
     if (!currentCategory) {
-      return state.products;
+      return products;//this is the destructured products state from GLOBAL redux state
     }
 
-    return state.products.filter(product => product.category._id === currentCategory);
+    return products.filter(product => product.category._id === currentCategory);
   }
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {state.products.length ? (
-        <div className="flex-row">
-            {filterProducts().map(product => (
-                <ProductItem
-                  key= {product._id}
-                  _id={product._id}
-                  image={product.image}
-                  name={product.name}
-                  price={product.price}
-                  quantity={product.quantity}
-                />
-            ))}
-        </div>
-      ) : (
-        <h3>You haven't added any products yet!</h3>
-      )}
-      { loading ? 
-      <img src={spinner} alt="loading" />: null}
+      {
+        products.length //global redux products state
+        ? 
+        (
+          <div className="flex-row">
+              {
+                filterProducts().map(product => (
+                  <ProductItem
+                    key= {product._id}
+                    _id={product._id}
+                    image={product.image}
+                    name={product.name}
+                    price={product.price}
+                    quantity={product.quantity}
+                  />
+                ))
+              }
+          </div>
+        ) 
+        : 
+        (
+          <h3>You haven't added any products yet!</h3>
+        )
+      }
+      { 
+        loading ? 
+        <img src={spinner} alt="loading" />: null
+      }
     </div>
   );
 }
