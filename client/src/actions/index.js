c8ce0d0e1ca92b345e2fec9a5b8f19aca17eca20
Jake Fullmer <w.jake.fullmer@gmail.com>
2021-01-18 00:18:22 -0700

    C:/Program Files/Git/actions/index.js finally added

---
+++ client/src/actions/index.js
@@ -0,0 +1,56 @@
+export const updateProducts = (products) => {
+    return {
+      type: 'UPDATE_PRODUCTS',
+      payload: products
+    }
+  }
+  
+  export const updateCategories = (categories) => {
+    return {
+      type: 'UPDATE_CATEGORIES',
+      payload: categories
+    }
+  }
+
+  export const updateCurrentCategory = (name) => {
+    return {
+      type: 'UPDATE_CURRENT_CATEGORY',
+      payload: name
+    }
+  }
+ 
+  export const addToCart = (data) => {
+    return {
+      type: 'ADD_TO_CART',
+      payload: data
+    }
+  }
+  
+  export const addMultipleToCart = (data) => {
+    return {
+      type: 'ADD_MULTIPLE_TO_CART',
+      payload: data
+    }
+  }
+  
+  export const removeFromCart = (_id) => {
+    return {
+      type: 'REMOVE_FROM_CART',
+      payload: _id
+    }
+  }
+  
+  export const updateCartQuantity = (item, quantity) => {
+    return {
+      type: 'UPDATE_CART_QUANTITY',
+      _id: item._id,
+      payload: quantity
+    }
+  }
+  
+  export const toggleCart = (data) => {
+    return {
+      type: 'TOGGLE_CART',
+      payload: data
+    }
+  }
